122d896dfbf1f793c6fe199bfc4529171a75c45f
Nitesh Meena <niteshmeena698@gmail.com>
2019-11-16 23:15:15 +0530

    Makes debugging easier

---
+++ debug.c
@@ -0,0 +1,25 @@
+#include "debug.h"
+
+int debug_depth = 0;
+
+void add_debug_padding()
+{
+	int i = 0;
+
+	for(i = 0 ; i < debug_depth ; i++)
+	{
+		printf("\t");
+	}
+}
+
+
+void debug_add_call_layer()
+{
+	debug_depth += 1;
+}
+
+
+void debug_remove_call_layer()
+{
+	debug_depth -= 1;
+}
